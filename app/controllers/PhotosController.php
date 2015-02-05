0db246ce3b3a1156958878ac85e6514cdd142f14
Pedro Guglielmo <pedro@rckt.com.br>
2015-02-05 16:35:14 -0200

    removendo as removidas

--- app/controllers/PhotosController.php
+++ app/controllers/PhotosController.php
@@ -18,10 +18,12 @@ class PhotosController extends \BaseController {
 	{
     $needle = Input::get("q");
 		$photos = Photo::where('name', 'LIKE', '%' . $needle . '%')
+      ->orWhere('deleted', '=', '0')
       ->orWhere('description', 'LIKE', '%' . $needle . '%')
       ->orWhere('imageAuthor', 'LIKE', '%' . $needle . '%')
       ->orWhere('state', 'LIKE', '%' . $needle . '%')
       ->orWhere('city', 'LIKE', '%' . $needle . '%')
+      ->take(20)
       ->get();
     return View::make('/search',['photos' => $photos, 'query'=>$needle]);
 	}
