d3569f00e3bffe94ac67d0741123f2c8621adcb1
Pedro Guglielmo <pedro@rckt.com.br>
2015-02-22 23:55:03 -0300

    avatar fau e inicio do login com facebook

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -16,3 +16,22 @@
 <!--[if lt IE 9]>
 <script src="{{ URL::to("/") }}/js/html5shiv.js"></script>
 <![endif]-->
+
+<!-- FACEBOOK -->
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '344371539091709',
+      xfbml      : true,
+      version    : 'v2.2'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
