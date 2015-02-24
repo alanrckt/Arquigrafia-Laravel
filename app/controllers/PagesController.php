41ed18df7c8b5bdf08f6997e925fdbf114f0fa10
rmmsilva <renato.massao@live.com>
2015-02-24 11:30:28 -0300

    Correção busca imagens

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -29,7 +29,6 @@ class PagesController extends BaseController {
 		if ($tag) {
 			// $byTag = $tag->photos;
 		}
-<<<<<<< HEAD
 
       $photos = Photo::where('deleted', '=', '0')
       			->where(function ($query) use($needle) {
@@ -41,16 +40,6 @@ class PagesController extends BaseController {
 			    	->orWhere('city', 'LIKE', '%' . $needle . '%');
       			})
       			->get();
-=======
-		$photos = Photo::orWhere('name', 'LIKE', '%' . $needle . '%')
-      ->orWhere('description', 'LIKE', '%' . $needle . '%')
-      ->orWhere('imageAuthor', 'LIKE', '%' . $needle . '%')
-			->orWhere('workAuthor', 'LIKE', '%' . $needle . '%')
-      ->orWhere('state', 'LIKE', '%' . $needle . '%')
-      ->orWhere('city', 'LIKE', '%' . $needle . '%')
-      ->andWhere('deleted', '=', '0')
-      ->get();
->>>>>>> fdd37d0d099bea55bcc245130aab653338d4a094
 		// $photos = $photos->merge($byTag);
     
     return View::make('/search',['tags' => $tags, 'photos' => $photos, 'query'=>$needle]);
