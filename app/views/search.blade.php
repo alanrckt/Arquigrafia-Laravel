247378b9a5255218979d2cd3c085fa812a31ed24
rmmsilva <renato.massao@live.com>
2015-02-24 11:47:49 -0300

    inclusão das imagens _home na pesquisa

--- app/views/search.blade.php
+++ app/views/search.blade.php
@@ -81,8 +81,10 @@
             <div class="item h<?php echo $size; ?>">
             	<div class="layer" data-depth="0.2">
                 <a href='{{ URL::to("/photos/{$photo->id}") }}'>
-                <img src='{{ URL::to("/arquigrafia-images/{$photo->id}_view.jpg") }}' title='{{ $photo->name }}'>
-                </a>
+                <?php if ($size==1) $path = '/arquigrafia-images/'. $photo->id . '_home.jpg'; 
+                 else $path = '/arquigrafia-images/'. $photo->id . '_view.jpg';?>
+                 <img src="{{ asset( $path ) }}" title="{{ $photo->name }}">
+                 </a>
                 <div class="item-title">{{ $photo->name }}</div>
             	</div>
             </div>
