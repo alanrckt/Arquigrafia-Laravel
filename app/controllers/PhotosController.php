c120ec8ba6695ece5bf754749b69ebaf34687590
anapaula <anapaula_spbr@yahoo.com.br>
2015-02-24 13:15:49 -0300

    Movida a variável input para o início do método para ser considerada na função de validação

--- app/controllers/PhotosController.php
+++ app/controllers/PhotosController.php
@@ -82,7 +82,7 @@ class PhotosController extends \BaseController {
     
   if ($validator->fails()) {
       $messages = $validator->messages();
-      return Redirect::to('/photos/form')->withErrors($messages);
+      return Redirect::to('/photos/form', ['photos' => $photos, 'owner' => $user])->withErrors($messages);
     } else {
 
     if (Input::file('photo')->isValid()) {
@@ -155,7 +155,7 @@ class PhotosController extends \BaseController {
 
     } else {
 	  $messages = $validator->messages();
-      return Redirect::to('/photos/form')->withErrors($messages);
+      return Redirect::to('/photos/form', ['photos' => $photos, 'owner' => $user])->withErrors($messages);
       //print_r(Input::all());
     }
  }
