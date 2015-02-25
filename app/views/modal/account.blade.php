efe1855235fd500ebae76b12aca0b61398b01d66
Pedro Guglielmo <pedro@rckt.com.br>
2015-02-25 02:25:15 -0300

    termos de compromisso

--- app/views/modal/account.blade.php
+++ app/views/modal/account.blade.php
@@ -46,8 +46,7 @@
           
           <div class="four columns alpha omega">
           
-            <!--<p>Li e aceito os <a href="/18/termsOfService" target="_blank" style="text-decoration: underline;">termos de compromisso</a>: {{ Form::checkbox('terms', 'read') }}</p>
-            -->
+            <p>Li e aceito os <a href="{{ URL::to('/terms') }}" target="_blank" style="text-decoration: underline;">termos de compromisso</a>: {{ Form::checkbox('terms', 'read') }}</p>
 			<p><a href="http://creativecommons.org/licenses/?lang=pt" id="creative_commons" style="text-decoration:underline;">Creative Commons</a></p>
             <p>{{ $errors->first('terms') }}</p>
           
