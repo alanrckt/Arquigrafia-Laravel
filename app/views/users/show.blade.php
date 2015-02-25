1c1e4d9f05b046f0cacb2db8a844ccf7ec32bfa4
anapaula <anapaula_spbr@yahoo.com.br>
2015-02-25 10:36:16 -0300

    Remoção de linhas para alterar tamanho da imagem

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -73,8 +73,8 @@
               $size = 1; 
               /*if ($i%5 == 3) $size = 2;
               if ($i%10 == 8) $size = 3;*/
-			  if ($i%7 == 6) $size = 2;
-              if ($i%21 == 6) $size = 3;
+			  //if ($i%7 == 6) $size = 2;
+              //if ($i%21 == 6) $size = 3;
             ?>
             
             <div class="item h<?php echo $size; ?>">
