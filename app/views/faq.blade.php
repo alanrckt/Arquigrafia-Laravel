638416616f4523ce5a97d20fb97cddfeba9a712e
Pedro Guglielmo <pedro@rckt.com.br>
2015-03-12 15:54:40 -0300

    contador do footer
    remoção das imagens delegadas

--- app/views/faq.blade.php
+++ app/views/faq.blade.php
@@ -40,29 +40,24 @@
         <br />
         
            <h3> Posso fazer uso comercial das imagens do Arquigrafia? </h3> 
-           Se a licença Creative Commons atribuída pelo autor da imagem permitir esse uso sim - Atribuição (BY) -, desde que seja dado crédito pela criação original. Se a licença restringir o uso comercial, não há permissão do autor para tanto, e eventuais usos indevidos estarão sujeitos às penalidades legais cabíveis.
-           <br />
-           <br />
+           <p>Se a licença Creative Commons atribuída pelo autor da imagem permitir esse uso sim - Atribuição (BY) -, desde que seja dado crédito pela criação original. Se a licença restringir o uso comercial, não há permissão do autor para tanto, e eventuais usos indevidos estarão sujeitos às penalidades legais cabíveis.</p>
         
            <h3> Posso utilizar as imagens do Arquigrafia em publicações? </h3> 
-           Se a publicação for comercializada, cabem os esclarecimentos refentes ao uso comercial de imagens do Arquigrafia. Se a publicação não for comercializada, o uso das imagens deve ser pautado pela licença Creative Commons atribuída pelo autor. Em todo caso, a autoria deve sempre ser mencionada.
-           <br />
-           <br />
+           <p>Se a publicação for comercializada, cabem os esclarecimentos refentes ao uso comercial de imagens do Arquigrafia. Se a publicação não for comercializada, o uso das imagens deve ser pautado pela licença Creative Commons atribuída pelo autor. Em todo caso, a autoria deve sempre ser mencionada.</p>
         
            <h3> Todas as imagens do Arquigrafia tem a mesma licença Creative Commons? </h3> 
-           Não, cada autor atribui uma licença específica às suas imagens, e pode mesmo atribuir licenças distintas para cada uma de suas imagens.
-           <br />
-           <br />
+           <p>Não, cada autor atribui uma licença específica às suas imagens, e pode mesmo atribuir licenças distintas para cada uma de suas imagens.</p>
         
            <h3> Se eu fizer um download de uma imagem do Acervo da Biblioteca da FAU no Arquigrafia, como posso reconhecer esta imagem depois, em meio a outras nos meus arquivos pessoais? </h3> 
-           Clicando com o botão direito do mouse sobre a imagem acesse ‘Propriedades’. Em seguida entre em ‘Detalhes’ e verifique a propriedade ‘Origem’, no qual constam os Direitos Autorais, e outros metadados.. A informação que consta neste campo irá mencionar FAU e depois a licença CC atribuída pelo autor ou por seus familiares, como por exemplo: FAU - Creative Commons 3.0 BY-NC-ND
-           <br />
-           <br />
+           <p>Clicando com o botão direito do mouse sobre a imagem acesse ‘Propriedades’. Em seguida entre em ‘Detalhes’ e verifique a propriedade ‘Origem’, no qual constam os Direitos Autorais, e outros metadados.. A informação que consta neste campo irá mencionar FAU e depois a licença CC atribuída pelo autor ou por seus familiares, como por exemplo: FAU - Creative Commons 3.0 BY-NC-ND</p>
         
            <h3> Como faço para remover uma fotografia que enviei? </h3> 
-           Entre em seu perfil de usuário e em ‘Minhas imagens’ clique sobre a imagem que deseja remover. Assim que a imagem for ampliada clique no ícone ‘Remover foto’. 
-           <br />
-           <br />
+           <p>Entre em seu perfil de usuário e em ‘Minhas imagens’ clique sobre a imagem que deseja remover. Assim que a imagem for ampliada clique no ícone ‘Remover foto’.</p>
+           
+           <h3>Posso criar um álbum com qualquer imagem do sistema?</h3>
+           <p>Sim, todas as imagens disponíveis no Arquigrafia podem ser organizadas em álbum. Entretanto, a autoria é alienável, a licença atribuída pelo autor deve ser respeitada e se eventualmente o autor retirar essa imagem do Arquigrafia, esta será retirada do seu álbum.</p>
+           
+           <br>
            
           <h3>Outras perguntas</h3>
           <p> Caso ainda tenha dúvidas entre contato com nossa equipe usando o email <a href="mailto:arquigrafiabrasil@gmail.com " target="_blank">arquigrafiabrasil@gmail.com</a>.</p>
