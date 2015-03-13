97cd606594e2ba31dd1cfba893cb24b68aa0a243
anapaula <anapaula_spbr@yahoo.com.br>
2015-03-12 21:47:38 -0300

    Inserção de url para edição de usuário

--- app/routes.php
+++ app/routes.php
@@ -38,6 +38,9 @@ Route::get('/users/logout', 'UsersController@logout');
 Route::get('users/login/fb', 'UsersController@facebook');
 Route::get('users/login/fb/callback', 'UsersController@callback');
 Route::resource('/users','UsersController');
+Route::get('/users/account/edit', 'UsersController@edit');
+Route::get('/users/update', 'UsersController@update');
+
 
 /* FOLLOW */
 Route::get('/friends/follow/{user_id}', 'UsersController@follow');
